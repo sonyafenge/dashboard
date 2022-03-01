007f4168dd08035a75f6fa1928aa5627bd171e4d
click2cloud-team5 <click2cloud-team5@click2cloud.net>
2022-03-01 15:50:20 +0530

    code for logo and title of dashboard

--- src/app/frontend/common/services/global/title.ts
+++ src/app/frontend/common/services/global/title.ts
@@ -1,20 +1,5 @@
-// Copyright 2017 The Kubernetes Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import {Injectable} from '@angular/core';
 import {Title} from '@angular/platform-browser';
-
 import {GlobalSettingsService} from './globalsettings';
 
 @Injectable()
@@ -37,7 +22,7 @@ export class TitleService {
   }
 
   private apply_(): void {
-    let title = 'Kubernetes Dashboard';
+    let title = 'Centaurus Dashboard';
 
     if (this.clusterName && this.clusterName.length > 0) {
       title = `${this.clusterName} - ` + title;
