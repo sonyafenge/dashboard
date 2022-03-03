17f4c94051e067222f8c8c5af5fad4f417305a8c
click2cloud-team5 <click2cloud-team5@click2cloud.net>
2022-03-03 16:43:54 +0530

    Bug fix for namespace and role dropdown

--- src/app/frontend/index.config.ts
+++ src/app/frontend/index.config.ts
@@ -1,23 +1,20 @@
+// Copyright 2017 The Kubernetes Authors.
+// Copyright 2020 Authors of Arktos - file modified.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import {InjectionToken} from '@angular/core';
 import {MatTooltipDefaultOptions} from '@angular/material';
-
-const username = sessionStorage.getItem('parentTenant');
-const userType = sessionStorage.getItem('userType');
-const defaultnamespace = sessionStorage.getItem('namespace');
-let tenant = '';
-if (userType === 'cluster-admin'){
-  tenant = 'system'
-} else {
-  tenant = username
-}
-
-let namespace = '';
-
-if (defaultnamespace !== '') {
-  namespace = defaultnamespace
-} else {
-  namespace = 'default'
-}
 
 export let CONFIG_DI_TOKEN = new InjectionToken<Config>('kd.config');
 
@@ -37,8 +34,8 @@ export const CONFIG: Config = {
   authTenantCookieName: 'tenant',
   csrfHeaderName: 'X-CSRF-TOKEN',
   skipLoginPageCookieName: 'skipLoginPage',
-  defaultNamespace: namespace,//'default',  //default
-  systemTenantName: tenant,  //system
+  defaultNamespace: 'default',
+  systemTenantName: 'system',
 };
 
 // Override default material tooltip values.
